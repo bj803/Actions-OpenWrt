9bbccecb95d3ebff4f4ebfc28d6f6d7be5d8cb8a
bj803 <109253859+bj803@users.noreply.github.com>
2022-10-06 09:02:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.111.3/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+
+# 修改机器名称
+sed -i "s/OpenWrt/Home803P/g" package/base-files/files/bin/config_generate
+
+# 修改密码
+# sed -i 's/root:::0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/base-files/files/etc/shadow
+#### 修改想要的root密码
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
